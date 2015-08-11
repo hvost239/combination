0c1751d66d0e75aa7eda0961389326acd72205da
Alexandr Khvastunov <alexandrhvastunov@gmail.com>
2015-08-11 16:03:08 +0300

    add big int test

--- Combination/MultithreadSolverTest.cpp
+++ Combination/MultithreadSolverTest.cpp
@@ -3,6 +3,8 @@
 #include <gtest\gtest.h>
 #include "MultithreadCnkSolver.h"
 
+#include <boost\multiprecision\cpp_int.hpp>
+
 class MultithreadSolverTest : public ::testing::Test{
 public:
 	MultithreadSolverTest()
@@ -52,6 +54,25 @@ TEST_F(MultithreadSolverTest, SimpleTestEq){
 
 
 	SimpleCnk<long long> simpleSolver;
+	for (auto& nk : datas){
+		ASSERT_EQ(solver(nk.first, nk.second), simpleSolver(nk.first, nk.second));
+	}
+}
+
+
+TEST(MultiThreadCnkSolverTest, BigInTest){
+
+	std::pair<boost::multiprecision::cpp_int, boost::multiprecision::cpp_int> datas[] = {
+		std::make_pair(1132, 834),
+		std::make_pair(23247, 2543),
+		std::make_pair(32343, 12367),
+		std::make_pair(56735, 43763)
+	};
+
+
+	SimpleCnk<boost::multiprecision::cpp_int> simpleSolver;
+	MultiThreadCnkSolver<boost::multiprecision::cpp_int> solver(4);
+
 	for (auto& nk : datas){
 		ASSERT_EQ(solver(nk.first, nk.second), simpleSolver(nk.first, nk.second));
 	}
